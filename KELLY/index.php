cc5516beaddf792bf8ecc920179836f761a3aa7a
leandro <100099447+carlosLGM@users.noreply.github.com>
2023-03-31 20:15:37 -0600

    Add files via upload

---
+++ KELLY/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Cumpleaños</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="javascript.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Barbie' rel='stylesheet'>
+</head>
+
+<body>
+    <header>
+        <h1>Feliz Cumpleaños Kelly</h1>
+        <img src="img/imagen3.png" alt="" width="100px">
+    </header>
+    <nav>
+        <ul>
+             
+        </ul>
+    </nav>
+    <main>
+        <section class="intro">
+            <h2></h2>
+            <h3>🥳🥳🥳🥳Feliz cumpleaños, Kelly! Espero que hoy te sientas muy querida y que este nuevo año de vida venga cargado de éxitos y buenos momentos. Que la vida te brinde muchos motivos para sonreír y que siempre tengas la fuerza y la determinación para seguir adelante. ¡Disfruta mucho tu día!🤍🤍🤍.</h3>
+            <center><img src="img/imagen1.png" alt=""  width="300px"></center>
+        </section>
+
+        <div class="audio-player">
+        <center><h3>playlist top del 1 de abril del 2004: </h3><center>  
+            <center><ul id="playlist">
+        <li class="active">
+           <a href="canciones/cancion1.mp3" _blank>
+           Britney Spears - Toxic
+
+        </a>
+        </li>
+        <li>
+            <a href="canciones/cancion2.mp3" _blank>
+            Peter Andre - Mysterious Girl
+
+            </a>
+        </li>
+        <li>
+            <a href="canciones/cancion3.mp3" _blank>
+            Chingy Featuring J. Weav - One Call Away
+            </a>
+        </li>
+        <li>
+            <a href="canciones/cancion4.mp3" _blank>
+            Jamelia - Thank You
+            </a>
+        </li>
+        <li>
+            <a href="canciones/cancion5.mp3" _blank>
+            Usher Featuring Lil Jon & Ludacris - Yeah!
+
+        </a>
+        </li>
+        </ul> </center>        
+        <img src="img/capy.gif"  alt="" width="300px">
+    </div>
+
+
+        <section class="galeria">
+            <h2></h2>
+            <section>
+    </main>
+    <footer>
+        <p>&copy;  Cumpleaños Feliz</p>
+    </footer>
+</body>
+
+</html>
